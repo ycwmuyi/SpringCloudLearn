a3c8c4dd77f3d5b506dc78cecb130ba2b8468f77
yangchengwei_home <chengwei.yang@urfresh.cn>
2017-11-08 00:21:01 +0800

    1.代码提交
    Signed-off-by: yangchengwei_home <chengwei.yang@urfresh.cn>

---
+++ service-feign/src/main/java/com/ycw/spring_cloud_learn/config/DisableHystrixConfiguration.java
@@ -0,0 +1,23 @@
+package com.ycw.spring_cloud_learn.config;
+
+import feign.Feign;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * Author: yangchengwei
+ * Date: 2017/11/08 0:02
+ * Description:
+ * History:
+ */
+@Configuration
+public class DisableHystrixConfiguration {
+
+    @Bean
+    @Scope("prototypeo")
+    public Feign.Builder feignBuilder(){
+        return Feign.builder();
+    }
+
+}
