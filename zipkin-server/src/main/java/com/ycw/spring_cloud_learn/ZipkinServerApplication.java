488389cb2b69e1da8f8198343c6f4a64bc833fa1
yangchengwei <yangchengwei@91zdan.com>
2017-12-06 00:03:21 +0800

    1.加入zipkin相关代码
    Signed-off-by: yangchengwei <yangchengwei@91zdan.com>

--- zipkin-server/src/main/java/com/ycw/spring_cloud_learn/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/ycw/spring_cloud_learn/ZipkinServerApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import zipkin.server.EnableZipkinServer;
 
-@EnableZuulProxy
-@EnableEurekaClient
+@EnableZipkinServer
 @SpringBootApplication
 public class ZipkinServerApplication {
 
